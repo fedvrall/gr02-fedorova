6d4b1dd39c17de7a255d295a0a9ef8192fcd2a36
fedvrall <mstonks@yandex.ru>
2021-10-25 14:42:53 +0000

    added extask at mark 4

---
+++ Extask16-b.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct record
+{
+    char drugName[64];
+    char indications[64];
+    int expYears;
+    int mfgYear;
+}; // 136 байт * n
+
+#define n 3
+struct record row[3];
+
+int main()
+{
+    for(int i = 0; i < n; i++)
+    { 
+        scanf("%s %s %d %d", 
+            row[i].drugName, row[i].indications, &row[i].expYears, &row[i].mfgYear);
+    }
+
+    printf("name\tindics\texp\tyear\n");
+    for(int i = 0; i < n; i++)
+    { 
+        printf("%s\t%s\t%d\t%d\n", 
+            row[i].drugName, row[i].indications, row[i].expYears, row[i].mfgYear);
+    }
+
+    FILE *printIn = fopen("Extask-b.txt", "w+");
+    for(int i = 0; i < n; i++)
+    { 
+        fprintf(printIn, "%s\t%s\t%d\t%d\n", 
+            row[i].drugName, row[i].indications, row[i].expYears, row[i].mfgYear);
+    }
+    fclose(printIn);
+
+    return 0;
+}
