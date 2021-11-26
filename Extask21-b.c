c11526f3dae0060e465036be57b56c632b0f849d
fedvrall <mstonks@yandex.ru>
2021-11-26 18:57:12 +0000

    Extask21 at mark 4

---
+++ Extask21-b.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+#define max_simb 256
+
+int main()
+{
+    FILE *f = fopen("Extask21-in.txt", "r");
+    FILE *f1 = fopen("Extask21-out1.txt", "w+");
+
+    int i = 1;
+
+    while(1)
+    {
+        char s[max_simb];
+        if(fgets(s, max_simb, f) == NULL)  
+            break;
+        char *c = strchr(s, '\n');
+        if (c != NULL)
+            *c = '\0';
+
+        fprintf(f1, "%d %s\n", i, s);
+
+        i++;
+    }
+
+    char str[15] = "total strings";
+    fprintf(f1, "%s %d\n", str, i-1);
+
+    fclose(f);
+    fclose(f1);
+
+    return 0;
+}
