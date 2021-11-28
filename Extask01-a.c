dc73e0dec64de3e767658f793398018a6d6d786e
fedvrall <mstonks@yandex.ru>
2021-11-28 18:16:56 +0000

    Extask01 at mark 3

---
+++ Extask01-a.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define max_word 64
+
+struct element
+{
+    char word[max_word];
+    int count;
+    struct element *next;
+};
+
+struct element *first = NULL;
+
+struct element *find(char word[max_word])
+{
+    struct element *current = first;
+    while (current != NULL)
+    {
+        if(strcmp(current->word, word) == 0)
+            return current;
+        current = current->next;
+    }
+    return NULL;
+}
+
+struct element *add()
+{
+    struct element *mem = (struct element *) malloc (sizeof(struct element));
+    mem -> next = first;
+    first = mem;
+    return mem;
+}
+
+int main()
+{
+    FILE *f = fopen("extask01-a.txt", "r");
+    while(1)
+    {
+        char temp[max_word];
+        int n = fscanf(f, "%s", temp);
+        if(n != 1)
+            break;
+        //printf("%s", temp);
+
+        struct element *found = find(temp);
+        if(found != NULL)
+            found -> count++;
+        else
+        {
+            struct element *added = add();
+            strcpy(added -> word, temp);
+            added -> count = 1;
+        }
+    }
+    fclose(f);
+
+    struct element *current = first;
+    while(current != NULL)
+    {
+        printf("%s %d\n", current->word, current->count);
+        struct element *next = current -> next;
+        free(current);
+        current = next;
+    }
+    return 0;
+}
