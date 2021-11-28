ab6015ad456dc4b915f6f3f12caf2046ff5e9936
fedvrall <mstonks@yandex.ru>
2021-11-28 07:46:12 +0000

    Extask11 at mark 3

---
+++ Extask11-a.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define rows 25
+#define cols 5
+
+#define markmin 2
+#define markmax 5
+
+int main()
+{
+    int tab[rows][cols];
+
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < cols; j++)
+            tab[i][j] = markmin + rand() % (markmax - markmin + 1);
+    
+    float avg[rows];
+
+    for(int i = 0; i < rows; i++)
+    {
+        int sum = 0;
+        avg[i] = 0.0f;
+        for(int j = 0; j < cols; j++)
+            sum += tab[i][j];
+        avg[i] = sum / (float) cols;
+    }
+
+    printf("num");
+    for(int j = 0; j < cols; j++)
+        printf("\t%d", j + 1);
+
+    printf("\tavg\n");
+
+    for(int i = 0; i < rows; i++)
+    {
+        printf("%d", i + 1);
+        for(int j = 0; j < cols; j++)
+            printf("\t%d", tab[i][j]);
+        printf("\t%.1f\n", avg[i]);
+    }
+
+    return 0;
+}
