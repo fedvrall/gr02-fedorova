fb1cfecfa2685d2c31ae102c8e188c52c4237d1f
fedvrall <mstonks@yandex.ru>
2021-11-29 09:57:12 +0000

    Extask15 at mark 4

---
+++ Extask15-b.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+
+struct record
+{
+    char os[32];
+    int virtMem;
+    int RAM;
+    int price;
+};
+
+#define rows 3
+struct record tab[rows];
+
+int main()
+{
+    char temp[32];
+    for(int i = 0; i < rows; i++)
+    {
+        printf("[record %d]\nos, virtMem, ram, price: ", i);
+        scanf("%s %d %d %d", tab[i].os, &tab[i].virtMem, &tab[i].RAM, &tab[i].price);
+
+        putchar('\n');
+    }
+
+    for(int i = 0; i < rows; i++)
+        printf("%s\t%d\t%d\t%d\n", tab[i].os, tab[i].virtMem, tab[i].RAM, tab[i].price);
+
+    char os[32];
+    printf("Найти ОС: ");
+    scanf("%s", os);
+    putchar('\n');
+    for(int i = 0; i < rows; i ++)
+    {
+        if(strcmp(tab[i].os, os) == 0)
+        {
+            printf("virtMem: %d\n", tab[i].virtMem);
+            printf("RAM: %d\n", tab[i].RAM);
+        }
+    }
+    putchar('\n');
+    printf("Хочу такую цену: ");
+    int price;
+    scanf("%d", &price);
+    int m = 0;
+    for(int i = 0; i < rows; i++)
+    {
+        if(price >= tab[i].price)  
+        {
+            printf("os: %s\n", tab[i].os);
+            printf("virtMem: %d\n", tab[i].virtMem);
+            printf("RAM: %d\n", tab[i].RAM);
+            printf("price: %d\n", tab[i].price);
+            m++;
+            putchar('\n');
+        }
+    }
+
+    if(m < 1)
+            printf("Извините, по вашим параметрам ничего не найдено\n");
+
+    return 0;
+}
